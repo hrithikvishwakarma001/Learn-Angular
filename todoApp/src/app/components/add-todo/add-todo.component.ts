137bda322194d3ba06a46ad0f81e36fa4c0302d5
Hrithik Vishwakarma <hritik8008@gmail.com>
2023-08-20 18:28:56 +0530

    created toggle service

--- todoApp/src/app/components/add-todo/add-todo.component.ts
+++ todoApp/src/app/components/add-todo/add-todo.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Todo } from 'src/app/Todo';
-
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
@@ -10,6 +11,12 @@ export class AddTodoComponent {
   @Output() onAddTodo: EventEmitter<Todo> = new EventEmitter();
   text!: string;
   reminder: boolean = false;
+  showAddTodo!: boolean;
+  subscription!: Subscription;
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTodo = value));
+  }
+
   onSubmit() {
     if (!this.text) {
       alert('Please add a task');
