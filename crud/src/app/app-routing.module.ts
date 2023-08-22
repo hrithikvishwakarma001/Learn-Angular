1d9aa05ef751ef25087cf8fdde1dc6249cce7be0
Hrithik Vishwakarma <hritik8008@gmail.com>
2023-08-22 13:04:37 +0530

    created routing and its functionality now its time to work in service part

--- crud/src/app/app-routing.module.ts
+++ crud/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { TodoListComponentComponent } from './components/todo-list-component/todo-list-component.component';
+import { CreateTodoComponentComponent } from './components/create-todo-component/create-todo-component.component';
+const routes: Routes = [
+  { path: 'todos', component: TodoListComponentComponent },
+  { path: 'create', component: CreateTodoComponentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
